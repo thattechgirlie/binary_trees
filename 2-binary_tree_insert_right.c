c28a26b5fdccf61035f0b4cf4b20aedd5b9a9fec
thattechgirlie <wanji.njoro1@gmail.com>
2024-03-28 01:01:09 -0700

    corrections

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,16 +12,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new = binary_tree_node(parent, value);
-	
+
 	if (new == NULL)
 		return (NULL);
-	
+
 	if (parent->right != NULL)
 	{
 		new->right = parent->right;
 		parent->right->parent = new;
 	}
-	
+
 	parent->right = new;
 	return (new);
 }
